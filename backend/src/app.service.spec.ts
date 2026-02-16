2ccee548b236b8868fd2ce7785645c7087750304
Octopicsus <chobotnicecz@gmail.com>
2026-02-16 17:54:41 +0100

    jest test fix

--- backend/src/app.service.spec.ts
+++ backend/src/app.service.spec.ts
@@ -1,6 +1,7 @@
 import { hrtime } from 'node:process';
 
 import { AppService, SYMBOLS } from './app.service';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AppService', () => {
     let appService: AppService;
@@ -10,10 +11,11 @@ describe('AppService', () => {
     });
 
     describe('generateMatrix()', () => {
-        it('size 0 generates empty array', () => {
-            const matrix = appService.generateMatrix(0, Date.now());
-            expect(matrix).toEqual([]);
+
+        it('size 0 throws BadRequestException', () => {
+            expect(() => appService.generateMatrix(0, Date.now())).toThrow(BadRequestException);
         });
+
         it('size 1 generates a single cell', () => {
             const matrix = appService.generateMatrix(1, Date.now());
             expect(matrix.length).toEqual(1);
